d52867618645502df5e988acdcb8acd1a5ec5b11
felipehenrique159 <felipecod6@hotmail.com>
2025-06-11 19:09:25 -0300

    feat: adicionado controller do analizador de texto

--- src/controllers/AnalyzeController.ts
+++ src/controllers/AnalyzeController.ts
@@ -1,9 +1,39 @@
 import { Request, Response } from 'express';
-
+import AnalyzeService from '../services/analyzeService';
 export default class AnalyzeController {
     static async analyzeText(request: Request, response: Response) {
-        return response.status(200).json({
-            message: 'teste',
-        });
+        try {
+            const { text } = request.body;
+
+            if (!text) {
+                return response.status(400).json({ error: "Text is required" });
+            }
+
+            const textAnalyze = await AnalyzeService.analyzeSentiment(text);
+
+            response.status(200).json(textAnalyze);
+        } catch (error) {
+            response.status(500).json({ error: 'Error analyzing text' });
+        }
+    }
+
+    static async searchTerm(request: Request, response: Response) {
+        try {
+            const term = (request.query.term as string)?.toLowerCase();
+
+            if (!term) {
+                return response.status(400).json({ error: "Term is required" });
+            }
+
+            const isFoundTerm = await AnalyzeService.searchTerm(term);
+
+            if (!isFoundTerm) {
+                return response.status(404).json({ error: "No analysis found for this term." });
+            }
+
+            response.json({ isFoundTerm });
+        } catch (error) {
+            response.status(500).json({ error: 'Error searching term' });
+        }
     }
 }
